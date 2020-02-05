3961ae162f4c761e68fe8e8829ff8c26aecd54ed
Marcos Dutra <mdutra@magnasistemas.com.br>
2020-02-05 14:47:38 -0300

    Dando inicio ao projeto

---
+++ inqueritoPolicial/src/main/java/br/com/magnasistemas/inqueritoPolicial/InqueritoPolicialApplication.java
@@ -0,0 +1,13 @@
+package br.com.magnasistemas.inqueritoPolicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InqueritoPolicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InqueritoPolicialApplication.class, args);
+	}
+
+}
---
+++ inqueritoPolicial/src/test/java/br/com/magnasistemas/inqueritoPolicial/InqueritoPolicialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.magnasistemas.inqueritoPolicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InqueritoPolicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
